2f3666fdd329e98a79621767862cf6cf0dd4e190
unknown <aguirreh8@gmail.com>
2018-04-15 19:06:37 -0400

    Add nav-tab links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Hero from "./components/Hero";
+import Container from "./components/Container"
 import "./App.css";
 
 const App = () => (
   <Router>
     <Wrapper>
       <Hero />
+      <Container>
+      	<ul className="nav nav-tabs">
+      		<li className="nav-item">
+      			<a href="" className='nav-link active'>News</a>
+      		</li>
+      		<li className="nav-item">
+      			<a href="" className="nav-link">Saved</a>
+      		</li>
+      	</ul>
+      </Container>
     </Wrapper>
   </Router>
 )
