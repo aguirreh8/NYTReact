0102c07a5ce7260eddfb1dda8b7c0908e8441871
unknown <aguirreh8@gmail.com>
2018-04-16 19:05:18 -0400

    Add News component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Container from "./components/Container";
 import NavTabs from "./components/NavTabs";
 import SearchForm from "./components/SearchForm";
+import News from "./components/News"
 import "./App.css";
 
 const App = () => (
@@ -14,7 +15,7 @@ const App = () => (
       <Container>
       	<SearchForm />
       	<NavTabs />
-      	<Route exact path="/" component={Wrapper} />
+      	<Route path="/news" component={News} />
       	<Route path="/saved" component={Wrapper} />
       </Container>
     </Wrapper>
---
+++ client/src/components/News/News.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class News extends Component {
+
+	render() {
+		return (
+			<div>
+				<ul className="list-group">
+					<li className="list-group-item">
+						<a href="">
+							<span className="text-primary">Hello</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		)
+	}
+}
+
+export default News;
